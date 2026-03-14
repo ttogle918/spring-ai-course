470234f5a7612f2e6e7128e676d691b36b7a0baf
ttogle918 <ttogle918@naver.com>
2026-03-14 15:14:36 +0900

    ch01: llm(ai) 연결 전, spring 기본 화면, frontend : thymeleaf 사용

--- ch01-spring-ai-project/src/main/java/com/example/demo/controller/HomeController.java
+++ ch01-spring-ai-project/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,14 +1,12 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
-    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
-
+  @GetMapping("/")
+  public String home() {
+    return "home";
+  }
 }
